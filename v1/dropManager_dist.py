8b878a0facfbce0570647a7e5341ac104e208c7d
Shiro Amurha <46139101+shiroamurha@users.noreply.github.com>
2022-02-27 15:02:22 -0300

    Update dropManager_dist.py

--- v1/dropManager_dist.py
+++ v1/dropManager_dist.py
@@ -5,7 +5,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from time import sleep
-#import requests
+
 import os
 
 
